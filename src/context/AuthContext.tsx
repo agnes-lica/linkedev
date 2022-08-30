950b8fc47824368cf49b1b25efcca3d68c0249ae
Agnes-lica <agnes.lica@gmail.com>
2022-08-30 15:26:51 -0400

    fix: fixing an error in AuthProvider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,42 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import api from "../services/api";
 
-
-function AuthProvider() 
-  return (
-    <AuthContext.Provider
-    
-    </AuthContext.Provider>
-  );
+function AuthProvider() {
+  return;
 }
 
 export default AuthProvider;
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
