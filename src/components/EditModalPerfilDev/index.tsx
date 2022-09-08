7c6f6e42c57086fd4921282c5685d7f68861ec18
Gabriel Martins <lima.gabriel@sou.ufac.br>
2022-09-08 00:08:22 -0500

    fix: modal edit default error image

--- src/components/EditModalPerfilDev/index.tsx
+++ src/components/EditModalPerfilDev/index.tsx
@@ -30,91 +30,62 @@ const EditModalProfileDev = () => {
   });
 
   return (
-    <Container>
-      <div className="modal">
-        <Header>
-          <img
-            className="vectorBack"
-            src="./vector_edit.svg"
-            alt="Voltar"
-            onClick={() => setEditModalDev(null)}
-          />
-          <h2>Editar Perfil</h2>
-        </Header>
-        <Form onSubmit={handleSubmit(editProfileDev)}>
-          <div className="divModal">
-            <div className="formdev">
-              <label htmlFor="name">Nome</label>
-              <input
-                type="text"
-                id="name"
-                defaultValue={user?.name}
-                {...register("name")}
-              />
-              <p>{errors.name?.message}</p>
+		<Container>
+			<div className="modal">
+				<Header>
+					<img
+						className="vectorBack"
+						src="./vector_edit.svg"
+						alt="Voltar"
+						onClick={() => setEditModalDev(null)}
+					/>
+					<h2>Editar Perfil</h2>
+				</Header>
+				<Form onSubmit={handleSubmit(editProfileDev)}>
+					<div className="divModal">
+						<div className="formdev">
+							<label htmlFor="name">Nome</label>
+							<input type="text" id="name" defaultValue={user?.name} {...register("name")} />
+							<p>{errors.name?.message}</p>
 
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                id="email"
-                defaultValue={user?.email}
-                {...register("email")}
-              />
-              <p>{errors.email?.message}</p>
-              <label htmlFor="title">Título / Objetivo</label>
-              <input
-                type="text"
-                id="title"
-                defaultValue={user?.title}
-                {...register("title")}
-              />
-              <p>{errors.title?.message}</p>
-              <label htmlFor="stacks">Stacks</label>
-              <input
-                type="text"
-                id="stacks"
-                defaultValue={user?.stacks}
-                {...register("stacks")}
-              />
-              <p>{errors.stacks?.message}</p>
-            </div>
+							<label htmlFor="email">Email</label>
+							<input type="text" id="email" defaultValue={user?.email} {...register("email")} />
+							<p>{errors.email?.message}</p>
+							<label htmlFor="title">Título / Objetivo</label>
+							<input type="text" id="title" defaultValue={user?.title} {...register("title")} />
+							<p>{errors.title?.message}</p>
+							<label htmlFor="stacks">Stacks</label>
+							<input type="text" id="stacks" defaultValue={user?.stacks} {...register("stacks")} />
+							<p>{errors.stacks?.message}</p>
+						</div>
 
-            <div className="formdev2">
-              <img
-                className="perfilDev"
-                defaultValue={user?.avatar_URL}
-                src={user?.avatar_URL}
-                alt="Perfil Dev"
-                onError={handleImageError}
-              />
+						<div className="formdev2">
+							<img
+								className="perfilDev"
+								defaultValue={user?.avatar_URL}
+								src={user?.avatar_URL || "../../assets/user.png"}
+								alt="Perfil Dev"
+								onError={handleImageError}
+							/>
 
-              <label htmlFor="url">Escolher url da imagem</label>
-              <input
-                type="text"
-                id="url"
-                defaultValue={user?.avatar_URL}
-                {...register("avatar_URL")}
-              />
+							<label htmlFor="url">Escolher url da imagem</label>
+							<input type="text" id="url" defaultValue={user?.avatar_URL} {...register("avatar_URL")} />
 
-              <p>{errors.avatar_URL?.message}</p>
+							<p>{errors.avatar_URL?.message}</p>
 
-              <label htmlFor="bio">Bio</label>
-              <textarea
-                id="bio"
-                defaultValue={user?.bio}
-                {...register("bio")}
-              />
-              <p>{errors.bio?.message}</p>
-            </div>
-          </div>
-          <div className="divButton">
-            <button className="buttonEdit" type="submit">
-              Atualizar
-            </button>
-          </div>
-        </Form>
-      </div>
-    </Container>
+							<label htmlFor="bio">Bio</label>
+							<textarea id="bio" defaultValue={user?.bio} {...register("bio")} />
+							<p>{errors.bio?.message}</p>
+						</div>
+					</div>
+					<div className="divButton">
+						<button className="buttonEdit" type="submit">
+							Atualizar
+						</button>
+					</div>
+				</Form>
+			</div>
+		</Container>
   );
 };
 
